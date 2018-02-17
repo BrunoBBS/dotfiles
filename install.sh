7d415795c58aa81aaad03c9d06fd47ed1eebe94a
Bruno BBS <bruno.bbs@hotmail.com>
2018-02-17 16:28:19 -0200

    Fix install script
    Fix intall script for installing plug.

--- install.sh
+++ install.sh
@@ -13,8 +13,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "done"
 
 echo "Installing Plug..."
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim<Paste>
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "done"
 
 echo "Installing oh-my-zsh"
